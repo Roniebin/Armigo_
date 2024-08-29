c7e2cb729d6455cb4f335e19698a811c7a6e32f0
JungHoonKim-KR <kik1232198@gmail.com>
2024-08-29 17:48:04 +0900

    SecurityConfig 설정 (cors)

--- src/main/java/org/example/virtualfittingshop/global/security/SecurityConfig.java
+++ src/main/java/org/example/virtualfittingshop/global/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -22,16 +23,12 @@ public class SecurityConfig {
                 httpSecurity
                         //cors
                         .cors(cors -> cors.configurationSource(request -> {
-                           var corsConfiguration =  new CorsConfiguration();
-                            corsConfiguration.setAllowedOrigins(List.of("*"));  // 모든 도메인 허용
-                            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // 모든 HTTP 메서드 허용
-                            corsConfiguration.setAllowedHeaders(List.of("*"));  // 모든 헤더 허용
-                            corsConfiguration.setAllowCredentials(true);  // 자격 증명(쿠키, Authorization 헤더 등) 허용
-
-
-                            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                            source.registerCorsConfiguration("/**", corsConfiguration);
-                            return corsConfiguration;
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowedHeaders(Collections.singletonList("*"));
+                            config.setAllowedMethods(Collections.singletonList("*"));
+                            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000")); // ⭐️ 허용할 origin
+                            config.setAllowCredentials(true);
+                            return config;
                         }))
                         .csrf((auth) -> auth.disable())
                         .authorizeHttpRequests(auth -> auth
